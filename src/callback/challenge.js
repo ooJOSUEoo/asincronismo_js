5adbbb8b54e75e3c097ceccbb8965e64c31302d8
Josue <sjosue5082002@gmail.com>
2023-03-01 12:47:53 -0600

    2.3 xmlhttprequest

---
+++ src/callback/challenge.js
@@ -0,0 +1,18 @@
+import { XMLHttpRequest } from 'xmlhttprequest';
+const API = 'https://api.escuela.co/api/v1'
+
+const fetchData = (url_api, callback) => {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = (event) => {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 200) {
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error(`Error ${url_api}`);
+                return callback(error, null);
+            }
+        }
+    }
+    xhttp.send();
+}
