897bb2fdcb61a191ae766ac1f29da8ae451e1163
Josue <sjosue5082002@gmail.com>
2023-03-12 14:14:03 -0500

    Qué son las promesas

---
+++ src/promise/index.js
@@ -0,0 +1,21 @@
+const promise = new Promise((resolve, reject) => {
+    resolve('Hola');
+});
+
+const cows = 15
+
+const constCows = new Promise((resolve, reject) => {
+    if (cows > 10) {
+        resolve('Hay muchas vacas ' + cows);
+    } else {
+        reject('No hay muchas vacas');
+    }
+});
+
+constCows.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+}).finally(() => {
+    console.log('Finalmente');
+});
